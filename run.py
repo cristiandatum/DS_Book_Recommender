8aad7847d86c2f3002c10400d07c6b8604aac1b7
Cristian Alberch <cristian.alberch@outlook.com>
2019-08-25 17:08:34 +0300

    libraries added

--- run.py
+++ run.py
@@ -1,1 +1,17 @@
 #Recommendation Engine
+import matplotlib.pyplot as plt
+
+import time
+import numpy as np
+import pandas as pd
+import random
+import operator
+from operator import itemgetter
+from statistics import mean 
+
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.cluster import KMeans
+
+df_ratings = pd.read_csv('df_ratings.csv' )
+df_books = pd.read_csv('df_books.csv' )
+df_tags = pd.read_csv('df_tags.csv' )
